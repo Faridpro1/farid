0d68609b88bbf659a75c6e749ee32eb6f9388d10
Faridpro1 <94738065+Faridpro1@users.noreply.github.com>
2022-04-01 19:44:15 -0700

    Update tapsiriq4.js

--- tapsiriq4.js
+++ tapsiriq4.js
@@ -1,17 +1,19 @@
 
 calculateSalesTax=[15, 17.5, 34]
 function calculateSalesTax() {
-     calculateSalesTax[0] + calculateSalesTax[0] * 20 % 100
-     calculateSalesTax[1] + calculateSalesTax[1] * 20 % 100
-     calculateSalesTax[2] + calculateSalesTax[2] * 20 % 100
+     calculateSalesTax[0] * 18 % 15
+     calculateSalesTax[1] * 21 / 17.5
+     calculateSalesTax[2] * 40 / 34
 }
-dollar =[calculateSalesTax[0] + calculateSalesTax[0] * 20 % 100, calculateSalesTax[1] + calculateSalesTax[1] * 20 % 100, calculateSalesTax[2] + calculateSalesTax[2] * 20 % 100]
+dollar = [calculateSalesTax[0] + calculateSalesTax[0] * 20 % 100, calculateSalesTax[1] + calculateSalesTax[1] * 20 % 100, calculateSalesTax[2] + calculateSalesTax[2] * 20 % 100]
 function formatCurrency() {
-    unsihft($).calculateSalesTax[0] + calculateSalesTax[0] * 20 % 100
-    unsihft($).calculateSalesTax[1] + calculateSalesTax[1] * 20 % 100
-    unsihft($).calculateSalesTax[2] + calculateSalesTax[2] * 20 % 100
+    unsihft($).calculateSalesTax[0] + calculateSalesTax[0]
+    unsihft($).calculateSalesTax[1] + calculateSalesTax[1]
+    unsihft($).calculateSalesTax[2] + calculateSalesTax[2]
 }
-
+console.log(calculateSalesTax[0]);
+console.log(calculateSalesTax[1]);
+console.log(calculateSalesTax[2]);
 /* ======= Aşağıdakı kodu dəyişməyin ===== */
 
 // Nəticə
